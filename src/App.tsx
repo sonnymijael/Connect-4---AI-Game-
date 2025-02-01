07f9110974ac9bcd0e00bb989698e11728b30317
Sonny Mijael <sonnymijael@gmail.com>
2025-01-31 22:20:14 -0600

    import correction

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import cn from 'classnames';
 import Confetti from 'react-confetti';
-import { CellType } from './core/enums';
+import { CellType } from './enums';
 
 interface AppState {
 	board: string[][];
